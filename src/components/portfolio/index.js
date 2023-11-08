10491861bdfbe0753d438e8001cc7bce94d736ee
Ihab Mahmoud <hakemehab22@gmail.com>
2023-11-08 22:35:51 +0300

    handled

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -7,7 +7,7 @@ import Loader from "react-loaders";
 
 const Portfolio = () => {
   const [className, setClassName] = useState("text-animate");
-    console.log(portfolioData.Portfolio);
+  console.log(portfolioData.Portfolio);
   useEffect(() => {
     const timer = setTimeout(() => {
       setClassName("text-animate-hover");
@@ -19,17 +19,17 @@ const Portfolio = () => {
     return (
       <div className="images-container">
         {data.map((image, idx) => {
-          if (image.source === "") {
-            return (
-              <div key={image.title} className="image-box">
-                <img
-                  src={image.cover}
-                  alt={image.title}
-                  className="portfolio-image"
-                />
-                <div className="content">
-                  <p className="title">{image.title} </p>
-                  <h4 className="description">{image.description}</h4>
+          return (
+            <div key={image.title} className="image-box">
+              <img
+                src={image.cover}
+                alt={image.title}
+                className="portfolio-image"
+              />
+              <div className="content">
+                <p className="title">{image.title} </p>
+                <h4 className="description">{image.description}</h4>
+                {image.url !== "" && (
                   <button
                     style={{
                       marginRight: "10px",
@@ -39,64 +39,21 @@ const Portfolio = () => {
                   >
                     Live Site
                   </button>
-                </div>
-              </div>
-            );
-          }else if(image.url === "") {
-            return (
-              <div key={image.title} className="image-box">
-                <img
-                  src={image.cover}
-                  alt={image.title}
-                  className="portfolio-image"
-                />
-                <div className="content">
-                  <p className="title">{image.title} </p>
-                  <h4 className="description">{image.description}</h4>
+                )}
+                {image.source !== "" && (
                   <button
+                    style={{
+                      marginRight: "10px",
+                    }}
                     className="btn"
-                    onClick={() => window.open(image.url)}
+                    onClick={() => window.open(image.source)}
                   >
                     Source Code
                   </button>
-                </div>
+                )}
               </div>
-            );
-          } else
-          {
-               return (
-                 <div key={image.title} className="image-box">
-                   <img
-                     src={image.cover}
-                     alt={image.title}
-                     className="portfolio-image"
-                   />
-                   <div className="content">
-                     <p className="title">{image.title} </p>
-                     <h4 className="description">{image.description}</h4>
-                     <button
-                       style={{
-                         marginRight: "10px",
-                       }}
-                       className="btn"
-                       onClick={() => window.open(image.url)}
-                     >
-                       Live Site
-                     </button>
-                     <button
-                       style={{
-                         marginRight: "10px",
-                       }}
-                       className="btn"
-                       onClick={() => window.open(image.source)}
-                     >
-                       Source
-                     </button>
-                   </div>
-                 </div>
-               );
-            }
-            
+            </div>
+          );
         })}
       </div>
     );
